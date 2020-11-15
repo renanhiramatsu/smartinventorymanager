a30185c3dc2a85a67e79d3134a36d4d9e9869329
Renan <renansilvahiramatsu@gmail.com>
2020-11-15 06:12:15 -0800

    Application commit.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
-
+#include<QGraphicsScene>
+#include <QGraphicsView>
 #include <QApplication>
+#include <QGraphicsPixmapItem>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    w.setWindowTitle("Inventario Smart");
+
+
     w.show();
     return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,33 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QPixmap>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap map("C:\\Users\\renan\\Desktop\\smarttec\\SMART_LOGOMARCAS\\smartlogo.png");
+    ui->imgViewLogo->setPixmap(map.scaled(200,80,Qt::KeepAspectRatio));
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+
+
+void MainWindow::on_pushButton_inserir_clicked()
+{
+  QString product_name = ui->lineEdit_produto->text();
+  QString repair_name = ui->lineEdit_reparo->text();
+  QString technician_name = ui->lineEdit_tecnico->text();
+
+
+}
+
 
